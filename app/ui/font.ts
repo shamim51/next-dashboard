5d12953f69d3222aa7b39eeb904e2f65f3bd990d
Shamim Ahmed <shamimahmed30082@gmail.com>
2024-08-24 21:10:07 +0600

    first commit

---
+++ app/ui/font.ts
@@ -0,0 +1,9 @@
+import {Inter, Lusitana} from 'next/font/google'
+// import { Jost } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
